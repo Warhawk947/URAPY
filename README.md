4a0e388fc448114fff70ad43450e9708e750197c
Warhawk947 <50366676+Warhawk947@users.noreply.github.com>
2020-04-15 16:08:38 -0400

    adding files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# URAPY
-A basic API for Uptime Robot
+# Introducing URAPY
+## What is URAPY?
+
+URAPY stands for **U**ptime **R**obot **API**, but I wanted it to be more cheesy so I switched it to **APY** (Get it? API? APY? Ok...)
+
+URAPY is a basic API wrapper for [Uptime Robot](https://uptimerobot.com/), allowing you to control monitors and public status pages via your python terminal!
+
+## How do I use URAPY?
+It's actually very simple
---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+  name='URAPY',
+  packages=['URAPY'],
+  version='1.0',
+  license='MIT',
+  description='A basic API for Uptime Robot',
+  author='Warhawk947',
+  author_email='not_telling_you@gmail.com',
+  url='https://github.com/Warhawk947/URAPY',
+  download_url='change_me',
+  keywords=['API', 'Uptime Robot'],
+  install_requires=['requests'],
+  classifiers=[
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',     
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+
+)
